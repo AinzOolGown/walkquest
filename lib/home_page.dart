d665487c35b10c62956101b810428ce177462de2
Bruno Rico <ricobruno58@gmail.com>
2026-04-22 19:00:30 -0400

    modifying home_page for step counter testing

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pedometer/pedometer.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 
 
@@ -11,124 +13,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _priceController = TextEditingController();
+  int _steps = 0;
+  late Stream<StepCount> _stepCountStream;
 
-  final CollectionReference _products =
-      FirebaseFirestore.instance.collection('products');
+  @override
+  void initState() {
+    super.initState();
+    initStepCounter();
+  }
 
-  Future<void> _createOrUpdate([DocumentSnapshot? documentSnapshot]) async {
-    String action = 'create';
-    if (documentSnapshot != null) {
-      action = 'update';
-      _nameController.text = documentSnapshot['name'];
-      _priceController.text = documentSnapshot['price'].toString();
+  Future<void> initStepCounter() async {
+    if (await Permission.activityRecognition.request().isGranted) {
+      _stepCountStream = Pedometer.stepCountStream;
+
+      _stepCountStream.listen(
+        onStepCount,
+        onError: onStepError,
+        cancelOnError: true,
+      );
+    } else {
+      print("Permission denied");
     }
-
-    await showModalBottomSheet(
-      isScrollControlled: true,
-      context: context,
-      builder: (BuildContext ctx) {
-        return Padding(
-          padding: EdgeInsets.only(
-            top: 20, left: 20, right: 20,
-            bottom: MediaQuery.of(ctx).viewInsets.bottom + 20,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                controller: _nameController,
-                decoration: const InputDecoration(labelText: 'Name'),
-              ),
-              TextField(
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                controller: _priceController,
-                decoration: const InputDecoration(labelText: 'Price'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                child: Text(action == 'create' ? 'Create' : 'Update'),
-                onPressed: () async {
-                  String name = _nameController.text;
-                  double price = double.parse(_priceController.text);
-                  if (name.isNotEmpty) {
-                    if (action == 'create') {
-                      await _products.add({"name": name, "price": price});
-                    }
-                    if (action == 'update') {
-                      await _products.doc(documentSnapshot!.id).update({
-                        "name": name, "price": price,
-                      });
-                    }
-                    _nameController.text = '';
-                    _priceController.text = '';
-                    Navigator.of(context).pop();
-                  }
-                },
-              )
-            ],
-          ),
-        );
-      },
-    );
   }
 
-  // TODO: Complete this function!
-  Future<void> _deleteProduct(String productId) async {
-    await _products.doc(productId).delete();
-    
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('You have successfully deleted a product')),
-    );
+  void onStepCount(StepCount event) {
+    setState(() {
+      _steps = event.steps;
+    });
   }
 
+  void onStepError(error) {
+    print("Step Count Error: $error");
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('CRUD Operations')),
-      body: StreamBuilder(
-        stream: _products.snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-          if (streamSnapshot.hasData) {
-            return ListView.builder(
-              itemCount: streamSnapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                final DocumentSnapshot documentSnapshot =
-                    streamSnapshot.data!.docs[index];
-                return Card(
-                  margin: const EdgeInsets.all(10),
-                  child: ListTile(
-                    title: Text(documentSnapshot['name']),
-                    subtitle: Text(documentSnapshot['price'].toString()),
-                    trailing: SizedBox(
-                      width: 100,
-                      child: Row(
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.edit),
-                            onPressed: () => _createOrUpdate(documentSnapshot),
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () => _deleteProduct(documentSnapshot.id),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            );
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _createOrUpdate(),
-        child: const Icon(Icons.add),
-      ),
+      
     );
   }
 }
