44989ac926127b01b2536c62edfe3ed3c5f0bf6a
Bruno Rico <ricobruno58@gmail.com>
2026-04-24 15:05:16 -0400

    setting up the guild page

---
+++ lib/guilds_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GuildsPage extends StatefulWidget {
+  const GuildsPage({super.key});
+
+  @override
+  State<GuildsPage> createState() => _GuildsPageState();
+}
+
+class _GuildsPageState extends State<GuildsPage> {
+
+
+
+}
