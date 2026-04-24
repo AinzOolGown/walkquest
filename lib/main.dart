96b6215d9d39c47ed2887ab340e522aa96c2ced0
Bruno Rico <ricobruno58@gmail.com>
2026-04-23 20:40:00 -0400

    prepping main for testing

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'auth/auth_wrapper.dart';
+import 'services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await NotificationService.init();
+  await NotificationService.requestPermission();
+  await NotificationService.scheduleRepeatingReminder();
   runApp(const MyApp());
 }
 
